5371be50e1f8dbea1fb35ca9efef303965d82e10
arwakh5897 <Arwa.muneer5@gmail.com>
2026-03-14 13:03:23 +0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,9 @@ import React, { useEffect } from "react";
 import Dashboard from "./pages/dashboard/dashboard";
 import Ad from "./components/header/ad";
 import Menu from "./components/header/menu";
-import Lenis from "lenis"; // ✅ default import
 
 const App = () => {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1,
-      smooth: true,
-    });
 
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  }, []);
 
   return (
     <div className="text-foreground bg-background">
