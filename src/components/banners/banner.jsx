ad0bd356176dd72167fb76adacf15cde6332e527
arwakh5897 <Arwa.muneer5@gmail.com>
2026-03-14 12:37:16 +0500

    coment

--- src/components/banners/banner.jsx
+++ src/components/banners/banner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Banner=()=>{
     return(
-        <div className="w-full h-full">
+        <div className="w-full h-full ">
                    <img className="w-full" src="assets/images/banner1.png"/>
         </div>
     )
