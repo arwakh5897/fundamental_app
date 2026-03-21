7739f8b4893ac5b5693c1ef52f109e10634b2da1
arwakh5897 <Arwa.muneer5@gmail.com>
2026-03-21 22:21:02 +0500

    Update filterUI.jsx
    discount-high-low

--- src/components/filterUI/filterUI.jsx
+++ src/components/filterUI/filterUI.jsx
@@ -16,6 +16,7 @@ const FilterUI = ({ sortType, setSortType }) => {
     { label: "Price: High to Low", value: "price-high-low" },
     { label: "Date: Old to New", value: "date-old-new" },
     { label: "Date: New to Old", value: "date-new-old" },
+   { label: "Discount: High to Low", value: "discount-high-low" },
   ];
 
   const activeLabel =
