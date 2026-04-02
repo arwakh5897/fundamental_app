73b86e9b0f91cbe6adbeb8825aeddecab7f4c28f
arwakh5897 <Arwa.muneer5@gmail.com>
2026-04-02 16:44:46 +0500

    d

--- src/components/products_details_components/product_cart_Components/cart_description.jsx
+++ src/components/products_details_components/product_cart_Components/cart_description.jsx
@@ -1,25 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 const CartDescription = ({ description }) => {
-  const [readMore, setReadMore] = useState(false);
-
-  // Split words
-  const words = description.split(" ");
-  const shortDescription =
-    words.length > 100 ? words.slice(0, 100).join(" ") + "..." : description;
-
+ 
+  const myDescription = description.split(" ");
+  const shortDescription = myDescription.length > 100 ? myDescription.slice(0, 100) + "...." : description;
   return (
-    <div className="text-xs lg:text-sm whitespace-pre-line">
-      <p>{readMore ? description : shortDescription}</p>
-      
-      {words.length > 100 && (
-        <button
-          className="text-blue-500 underline ml-1 text-xs"
-          onClick={() => setReadMore(!readMore)}
-        >
-          {readMore ? "Read Less" : "Read More"}
-        </button>
-      )}
+    <div>
+      <p className="text-xs lg:text-sm whitespace-pre-line">{shortDescription}</p>
     </div>
   );
 };
--- src/components/products_details_components/products_description&review/full_decription.jsx
+++ src/components/products_details_components/products_description&review/full_decription.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 const FullDescription =({ products})=>{
 
     return(
-        <div id="description" className="text-xs md:text-sm whitespace-pre-line">
+        <div id="description" className="text-sm whitespace-pre-line">
             <p>{products?.description}</p>
         </div>
     )
