81803e778de43c1b4d330ed57eefab036b0c96b6
arwakh5897 <Arwa.muneer5@gmail.com>
2026-04-05 20:38:18 +0500

    Update ad.jsx
    text

--- src/components/header/ad.jsx
+++ src/components/header/ad.jsx
@@ -13,7 +13,7 @@ const Ad = () => {
       <div className="marquee-wrapper">
         <div className="marquee marquee-track">
           {loopData.map((item,index)=>(
-          <section key={index} className="flex md:mx-36 2xl:mx-80 mx-12 gap-4 text-xs">
+          <section key={index} className="flex md:mx-36 2xl:mx-80 mx-12 gap-4 text-[5px] md:text-xs">
             <span >{item.name}</span>
             <a className="underline" href="/pages/shop_all">{item.btn_text}</a>
           </section>
