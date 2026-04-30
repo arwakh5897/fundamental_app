61d771fda12822d873322f18458d2f804aef9f29
arwakh5897 <Arwa.muneer5@gmail.com>
2026-04-30 06:08:52 +0500

    Update copy_right.jsx

--- src/components/footer/footer_components/copy_right.jsx
+++ src/components/footer/footer_components/copy_right.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CopyRight = () => {
     return (
-        <div className="text-center text-gray-600 mt-4">
+        <div className="text-center text-gray-600 text-xs mt-4">
             <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
         </div>
     );
