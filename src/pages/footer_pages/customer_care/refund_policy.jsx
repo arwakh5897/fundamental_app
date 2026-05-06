d94c7993cdbc94df11d4f52e2e3f9a17bfa4f191
arwakh5897 <Arwa.muneer5@gmail.com>
2026-05-06 20:10:34 +0500

    Update refund_policy.jsx

--- src/pages/footer_pages/customer_care/refund_policy.jsx
+++ src/pages/footer_pages/customer_care/refund_policy.jsx
@@ -5,7 +5,7 @@ const RefundPolicy = () => {
     <div className="bg-background text-foreground">
 
       {/* HERO */}
-        <h1 className="bg-footer lg:text-2xl font-bold text-center py-2 lg:py-4">Refund Policy</h1>
+        <h1 className="bg-menu lg:text-2xl font-bold text-center py-2 lg:py-4">Refund Policy</h1>
 
 
       {/* CONTENT */}
