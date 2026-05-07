7b738c5b6eb911db348d6e65c9cf56cbea0309f6
arwakh5897 <Arwa.muneer5@gmail.com>
2026-05-07 07:51:54 +0500

    Update card.jsx

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -41,7 +41,7 @@ const Card = ({ id, image, title, description ,category,
       {/* ✅ Card */}
       <div
         onClick={handleClick}
-        className="bg-card w-36 lg:w-72 shadow-sm overflow-hidden cursor-pointer flex flex-col rounded-xl relative group"
+        className="bg-card w-36 md:w-72 shadow-sm overflow-hidden cursor-pointer flex flex-col rounded-xl relative group"
       >
         {/* 👁 Eye Button */}
         {stock > 0 && (
