03dc9af4d1f283a233bc1cc96b7eaddf93dc8273
arwakh5897 <Arwa.muneer5@gmail.com>
2026-05-09 17:03:11 +0500

    Update scrolling_Products.jsx

--- src/components/products/scrolling_Products.jsx
+++ src/components/products/scrolling_Products.jsx
@@ -7,7 +7,7 @@
 
 //   const scrollReference = useRef(null);
 //     return(
-//         <div className="relative flex w-full items-center bg-black p-4">
+//         <div className="relative flex w-full items-center p-4">
 //         <LeftArrow scrollRef={scrollReference} />
 //           <div ref={scrollReference} 
 //           className="flex flex-row scrollbar-hide overflow-x-auto scroll-smooth lg:gap-8 gap-4 py-4">
