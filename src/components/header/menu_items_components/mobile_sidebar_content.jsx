798b6834f0ab46820be41305948026e8ebd17e18
arwakh5897 <Arwa.muneer5@gmail.com>
2026-05-09 14:31:57 +0500

    menu

--- src/components/header/menu_items_components/mobile_sidebar_content.jsx
+++ src/components/header/menu_items_components/mobile_sidebar_content.jsx
@@ -53,12 +53,12 @@ const MobileSidebarContent = ({ onClose }) => {
       <div className="flex gap-2 mb-4 px-5">
         <div 
         onClick={handleNewArrivalsClick}
-        className="flex-1 bg-white/20 hover:cursor-pointer rounded-xl px-3 py-2 text-xs text-center">
+        className="flex-1 bg-halfWhite hover:cursor-pointer rounded-xl px-3 py-2 text-xs text-center">
           New Drops
         </div>
         <div 
         onClick={handleTrendingClick}
-        className="flex-1 bg-white/20 hover:cursor-pointer rounded-xl px-3 py-2 text-xs text-center">
+        className="flex-1 bg-halfWhite hover:cursor-pointer rounded-xl px-3 py-2 text-xs text-center">
           Trending
         </div>
       </div>
@@ -72,7 +72,7 @@ const MobileSidebarContent = ({ onClose }) => {
             SHOP
           </p>
 
-          <div className="space-y-2 bg-white/20 p-2 rounded-2xl">
+          <div className="space-y-2 bg-halfWhite p-2 rounded-2xl">
             <Link to="/pages/shop_all" onClick={onClose} className={item + " hover-bg-box"}>
               <span>Shop All</span>
               <span className="text-xs text-white/30">→</span>
@@ -91,7 +91,7 @@ const MobileSidebarContent = ({ onClose }) => {
             EXPLORE
           </p>
 
-          <div className="space-y-2 bg-white/20 p-2 rounded-2xl">
+          <div className="space-y-2 bg-halfWhite p-2 rounded-2xl">
 
             <div
               onClick={() => setActiveMenu("concern")}
@@ -117,7 +117,7 @@ const MobileSidebarContent = ({ onClose }) => {
             BROWSE
           </p>
 
-          <div className="space-y-2 bg-white/20 p-2 rounded-2xl">
+          <div className="space-y-2 bg-halfWhite p-2 rounded-2xl">
 
             <div className={item + " cursor-pointer hover-bg-box"}>
               <span>Accessories</span>
