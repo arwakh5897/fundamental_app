fd2042d2184ddfc5872c4662d21ae797e4abf873
arwakh5897 <Arwa.muneer5@gmail.com>
2026-05-10 11:21:55 +0500

    Update scrolling_Products.jsx

--- src/components/products/scrolling_Products.jsx
+++ src/components/products/scrolling_Products.jsx
@@ -49,10 +49,10 @@ const ScrollingProducts = ({ allItems }) => {
             slidesPerView: 2,
           },
           640: {
-            slidesPerView: 5,
+            slidesPerView: 4,
           },
           768: {
-            slidesPerView: 4,
+            slidesPerView: 5,
           },
           1024: {
             slidesPerView: 4,
