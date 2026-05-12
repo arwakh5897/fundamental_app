8deff49fe92013fb83252529e7acbfbd2dcc92f1
arwakh5897 <Arwa.muneer5@gmail.com>
2026-05-12 12:56:03 +0500

    c

--- src/components/titles/ance.jsx
+++ src/components/titles/ance.jsx
@@ -65,7 +65,7 @@ const Ance = () => {
             <div className="flex flex-wrap gap-3 mt-5 sm:mt-7">
               <button
                 onClick={() => navigate("/pages/shop_all")}
-                className="mt-5 px-5 sm:px-6 py-2.5 sm:py-3
+                className="px-5 sm:px-6 py-2.5 sm:py-3
                 rounded-full text-xs sm:text-sm font-semibold
                 bg-buttons
                 hover-bg-buttons
