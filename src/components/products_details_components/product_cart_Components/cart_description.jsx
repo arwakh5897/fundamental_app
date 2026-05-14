1eef4f67c6f29c8403e5f0033a73963e177687e4
arwakh5897 <Arwa.muneer5@gmail.com>
2026-05-14 23:00:34 +0500

    Update cart_description.jsx

--- src/components/products_details_components/product_cart_Components/cart_description.jsx
+++ src/components/products_details_components/product_cart_Components/cart_description.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const CartDescription = ({ description = "" }) => {
  
   const myDescription = description.split(" ");
-  const shortDescription = myDescription.length > 80 ? myDescription.slice(0, 80).join(" ") + "..." : description;
+  const shortDescription = myDescription.length > 40 ? myDescription.slice(0, 40).join(" ") + "..." : description;
   return (
     <div>
       <p className="text-xs lg:text-sm whitespace-pre-line">{shortDescription}</p>
